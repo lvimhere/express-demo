baf277c804837f4a290847cd69b768453476fea7
sunjie <levy.sun@bestwehotel.com>
2018-03-26 18:32:41 +0800

    [update]更改目录结构，增加测试

--- src/app.js
+++ src/app.js
@@ -1,17 +1,41 @@
 let express = require('express')
 let handlebars = require('express-handlebars')
-// let fortune = require('./lib/fortune.js')
+let path = require('path')
+let fortune = require('./lib/fortune.js')
 let app = express()
+app.use(express.static(path.resolve(__dirname, './public')))
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.set('port', process.env.PORT || 3000)
+app.use((req, res, next) => {
+  res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1'
+  console.log(app.get('env'))
+  console.log(res.locals)
+  next()
+})
 app.get('/', (req, res) => {
   // res.send('hello world')
+  // console.log(req.url)
+  // console.log(req.method)
+  // console.log(req.header)
+  // console.log(req.headers)
+  // console.log(req.route)
+  // console.log(req.route.stack)
   res.render('home')
 })
-// app.get('/about', (req, res) => {
-//   res.render('./views/about', { fortune: fortune.getFortune() })
-// })
+app.get('/about', (req, res) => {
+  res.render('about', {
+    fortune: fortune.getFortune(),
+    pageTestScript: '/qa/tests-about.js'
+  })
+})
+
+app.get('/tours/hood-river', (req, res) => {
+  res.render('tours/hood-river')
+})
+app.get('tours/request-group-rate', (req, res) => {
+  res.render('tours/request-group-rate')
+})
 
 app.listen(app.get('port'), 'localhost', () => {
   console.log(
---
+++ src/public/qa/crossPageTest.js
@@ -0,0 +1,40 @@
+// var Browser = require('phantom'),
+//   assert = require('chai').assert;
+// var browser;
+// suite('Cross-Page Tests', function () {
+//   setup(function () {
+//     const instance = await phantom.create();
+//     const page = await instance.createPage();
+//   });
+//   test('requesting a group rate quote from the hood river tour page' +
+//     'should populate the referrer field',
+//     function (done) {
+//       var referrer = 'http://localhost:3000/tours/hood-river';
+//       page.open(referrer, function () {
+//         browser.clickLink('.requestGroupRate', function () {
+//           assert(browser.field('referrer').value === referrer);
+//           done();
+//         });
+//       });
+//     });
+//   test('requesting a group rate from the oregon coast tour page should ' +
+//     'populate the referrer field',
+//     function (done) {
+//       var referrer = 'http://localhost:3000/tours/oregon-coast';
+//       browser.visit(referrer, function () {
+//         browser.clickLink('.requestGroupRate', function () {
+//           assert(browser.field('referrer').value === referrer);
+//           done();
+//         });
+//       });
+//     });
+//   test('visiting the "request group rate" page dirctly should result ' +
+//     'in an empty referrer field',
+//     function (done) {
+//       browser.visit('http://localhost:3000/tours/request-group-rate',
+//         function () {
+//           assert(browser.field('referrer').value === '');
+//           done();
+//         });
+//     });
+// });
